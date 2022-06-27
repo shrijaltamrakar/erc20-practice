8d29bf3e7eacc9ef7b08450832d020cb8c6030cf
Shrijal Tamrakar <shrijal@gurzu.com>
2022-06-27 21:57:50 +0545

    Add function to transfer

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -7,6 +7,15 @@ contract ERC20 {
   string public name;
   string public symbol;
 
+  // mapping(address => uint256) private balances;
+
+  // function balanceOf(address owner) public view returns(uint256) {
+  //   return balances[owner];
+  // }
+
+  mapping(address => uint256) public balanceOf;
+
+
   constructor(string memory _name, string memory _symbol) {
     name = _name;
     symbol = _symbol;
@@ -15,4 +24,23 @@ contract ERC20 {
   function decimals() external pure returns (uint8) {
     return 18;
   }
+
+  //recipient -> where the caller want to transfer his funds to?
+  
+  function transfer(address recipient, uint256 amount) external returns (bool){
+    require(recipient!= address(0), "ERC20 transfer to the zero address");
+
+    // this will automatically calc asd hash of msg.sender address to look up balance value in storge
+    uint256 senderBalance = balanceOf[msg.sender];
+
+    require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
+
+    balanceOf[msg.sender] = senderBalance - amount;
+
+    balanceOf[recipient] += amount;
+
+    return true;
+  }
+
+
 }
